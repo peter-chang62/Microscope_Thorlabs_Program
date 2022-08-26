52273395df18391c50875fd5e55f8368852e3ce9
Peter <pech6593@colorado.edu>
2022-08-26 16:43:03 -0600

    finished all the triggering stuff!

--- MotorClassFromAptProtocolConnor.py
+++ MotorClassFromAptProtocolConnor.py
@@ -1,4 +1,6 @@
 import struct
+import time
+
 from APT import _auto_connect
 import numpy as np
 import APT as apt
@@ -14,29 +16,63 @@ class AptMotor(apt.KDC101_PRM1Z8):
         self.ENC_CNT_MM_S = self.ENC_CNT_MM * self.SAMPLING_INTERVAL * self.VEL_SCALING_FACTOR
         self.ENC_CNT_MM_S2 = self.ENC_CNT_MM * self.SAMPLING_INTERVAL ** 2 * self.VEL_SCALING_FACTOR
 
+        self.set_trigger_io_params(1, 10)
+        self._trigger_on = False
+
+        self.step_mm = 0.01
+        self.pulse_width_ms = 1
+
+    @property
+    def trigger_on(self):
+        return self._trigger_on
+
+    @trigger_on.setter
+    def trigger_on(self, flag):
+        assert isinstance(flag, bool)
+
+        # If turning trigger_on to false and trigger_on was originally true, turn off the trigger for the K-cube. If
+        # the user is turning the trigger on, this will be caught whenever the position is changed.
+        if (not flag) and self.trigger_on:
+            self.set_trigger_io_params(1, 10)
+
+        # update trigger_on
+        self._trigger_on = flag
+
     @_auto_connect
-    def position(self, position=None):
-        if position is None:
-            # Get the current position
-            # MGMSG_MOT_REQ_POSCOUNTER 0x0411
-            write_buffer = struct.pack("<BBBBBB", 0x11, 0x04,
-                                       0x01, 0x00,
-                                       self.dst, self.src)
-            self.write(write_buffer)
-            # MGMSG_MOT_GET_POSCOUNTER 0x0412
-            read_buffer = self.read(0x12, 0x04, req_buffer=write_buffer)
-            result = struct.unpack("<BBBBBBHl", read_buffer)
-            position = result[7] / self.ENC_CNT_MM
-            return position
-        else:
-            # Calculate the encoder value
-            enc_cnt = int(round(position * self.ENC_CNT_MM))
-            # MGMSG_MOT_MOVE_ABSOLUTE
-            write_buffer = struct.pack('<BBBBBBHl', 0x53, 0x04,
-                                       0x06, 0x00,
-                                       self.dst | 0x80, self.src,
-                                       0x0001, enc_cnt)
-            self.write(write_buffer)
+    def get_position(self):
+        # Get the current position
+        # MGMSG_MOT_REQ_POSCOUNTER 0x0411
+        write_buffer = struct.pack("<BBBBBB", 0x11, 0x04,
+                                   0x01, 0x00,
+                                   self.dst, self.src)
+        self.write(write_buffer)
+        # MGMSG_MOT_GET_POSCOUNTER 0x0412
+        read_buffer = self.read(0x12, 0x04, req_buffer=write_buffer)
+        result = struct.unpack("<BBBBBBHl", read_buffer)
+        position = result[7] / self.ENC_CNT_MM
+        return position
+
+    @_auto_connect
+    def set_position(self, value_mm):
+        # set trigger parameters if relevant
+        if self.trigger_on:
+            pos = self.get_position()
+
+            if pos < value_mm:  # going forward
+                self.set_trigger_pos_params(pos, value_mm, self.step_mm, self.pulse_width_ms, 13)
+            elif pos > value_mm:  # going backwards
+                self.set_trigger_pos_params(value_mm, pos, self.step_mm, self.pulse_width_ms, 14)
+            else:  # current position is already the target position
+                pass  # do nothing
+
+        # Calculate the encoder value
+        enc_cnt = int(round(value_mm * self.ENC_CNT_MM))
+        # MGMSG_MOT_MOVE_ABSOLUTE
+        write_buffer = struct.pack('<BBBBBBHl', 0x53, 0x04,
+                                   0x06, 0x00,
+                                   self.dst | 0x80, self.src,
+                                   0x0001, enc_cnt)
+        self.write(write_buffer)
 
     @_auto_connect
     def move_relative(self, rel_position):
@@ -168,7 +204,8 @@ class AptMotor(apt.KDC101_PRM1Z8):
         return start_fwd, interval_fwd, num_fwd, start_rev, interval_rev, num_rev, width_ms, num_cycles
 
     @_auto_connect
-    def set_trigger_pos_params(self, start_mm, stop_mm, step_mm, width_ms):
+    def set_trigger_pos_params(self, start_mm, stop_mm, step_mm, width_ms,
+                               trigger_mode=15):
         # MGMSG_MOT_SET_KCUBEPOSTRIGPARAMS 0x0526
         header = struct.pack("<6B", 0x26, 0x05, 0x2E, 0x00, 0xD0, self.src)
 
@@ -177,7 +214,7 @@ class AptMotor(apt.KDC101_PRM1Z8):
         step_enc = int(np.round(step_mm * self.ENC_CNT_MM))
         num_pulses = int(np.floor(abs(start_enc - stop_enc) / step_enc))
         width_us = int(np.round(width_ms * 1e3))
-        num_cycle = 1
+        num_cycle = 2147483647
 
         data = struct.pack("<H8l", 1, start_enc, step_enc, num_pulses, stop_enc,
                            step_enc, num_pulses, width_us, num_cycle)
@@ -185,6 +222,13 @@ class AptMotor(apt.KDC101_PRM1Z8):
         reserved = struct.pack("<3l", 0, 0, 0)
         write_buffer = header + data + reserved
         self.write(write_buffer)
+
+        if trigger_mode == 13:
+            self.set_trigger_io_params(1, 13)
+        if trigger_mode == 14:
+            self.set_trigger_io_params(1, 14)
+        if trigger_mode == 15:
+            self.set_trigger_io_params(1, 15)
 
 
 class KDC101(AptMotor):
@@ -205,12 +249,12 @@ class KDC101(AptMotor):
 
     @property
     def position(self):
-        return super().position()
+        return super().get_position()
 
     @position.setter
     def position(self, value_mm):
         # assuming it's in millimeters
-        super().position(position=value_mm)
+        super().set_position(value_mm)
 
     def move_to(self, value_mm):
         self.position = value_mm
@@ -226,3 +270,17 @@ class KDC101(AptMotor):
 
     def get_stage_axis_info(self):
         return 0., 12., "mm", None
+
+
+# %% ___________________________________________________________________________________________________________________
+m = KDC101('com5')
+m.trigger_on = True
+m.step_mm = 0.01
+m.pulse_width_ms = 1
+
+m.set_max_vel(.1)
+m.position = 0
+m.stop_profiled()
+
+# m.get_trigger_io_params()
+# m.get_trigger_pos_params()
